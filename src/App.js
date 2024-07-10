f92b6c7f45cccba4e3ebc0788dcf918c9f5ce3e5
Bsh <dnfg12@naver.com>
2024-07-10 16:12:41 +0900

    react masterclass first section study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const RotationAnimation = keyframes`
+  0%{
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100%{
+    transform:rotate(720deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 48px;
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${RotationAnimation} 2s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box>
+        <Emoji>😍</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
