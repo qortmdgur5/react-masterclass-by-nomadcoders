61c90e982c1a815e755bee797fb2bcfec6c6f7f3
Bsh <dnfg12@naver.com>
2024-07-11 14:45:55 +0900

    react masterclass third section typescript drawing circle and use type inerface

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="tomato" bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
