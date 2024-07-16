225fcf4623fc8cbba251789a92829a9a60493d16
Bsh <dnfg12@naver.com>
2024-07-16 20:58:26 +0900

    react masterClass section five 5.13 Lecture

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,77 @@
-function Chart() {
-    return <h1>Chart</h1>
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorycal {
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+
+interface ChartProps {
+    coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+    const { isLoading, data } = useQuery<IHistorycal[]>(["ohlcv", coinId], () =>
+        fetchCoinHistory(coinId)
+    );
+    return (
+        <div>
+            {isLoading ? (
+                "Loading chart..."
+            ) : (
+                <ApexChart
+                    type="line"
+                    series={[
+                        {
+                            name: "Price",
+                            data: data?.map((price) => price.close) ?? [],
+                        },
+                    ]}
+                    options={{
+                        theme: {
+                            mode: "dark",
+                        },
+                        chart: {
+                            height: 500,
+                            width: 500,
+                            toolbar: {
+                                show: false,
+                            },
+                            background: "transparent",
+                        },
+                        grid: {
+                            show: false
+                        },
+                        stroke: {
+                            curve: "smooth",
+                            width: 4,
+                        },
+                        yaxis: {
+                            show: false
+                        },
+                        xaxis: {
+                            axisBorder: {
+                                show: false,
+                            },
+                            axisTicks: {
+                                show: false,
+                            },
+                            labels: {
+                                show: false,
+                            }
+                        }
+                    }}
+                />
+            )}
+        </div>
+    );
 }
 
 export default Chart;
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
+const BASE_NICO_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`)
@@ -14,6 +15,14 @@ export function fetchCoinInfo(coinId: string) {
 
 export function fetchCoinTickers(coinId: string) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7;
+  return fetch(`${BASE_NICO_URL}?coinId=${coinId}`).then((response) =>
     response.json()
   );
 }
