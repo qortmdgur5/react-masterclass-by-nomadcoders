3e4ff46a3fc494a88a1850ab32e918efdc50ac96
Bsh <dnfg12@naver.com>
2024-07-17 16:18:15 +0900

    react masterClass section Six 6.2 Lecture

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Router from "./Router";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { darkTheme, lightTheme } from "./theme";
 import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "./atoms";
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -66,12 +68,10 @@ a {
 `;
 
 function App() {
-	const [isDark, setIsDark] = useState(false);
-	const toggleDark = () => setIsDark((current) => !current);
+	const isDark = useRecoilValue(isDarkAtom);
 	return (
 		<>
 			<ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-				<button onClick={toggleDark}>Toggle Mode</button>
 				<GlobalStyle />
 				<Router />
 				<ReactQueryDevtools />
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "./api";
 import ApexChart from "react-apexcharts";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "../atoms";
 
 interface IHistorycal {
     time_open: string;
@@ -18,6 +20,8 @@ interface ChartProps {
 }
 
 function Chart({ coinId }: ChartProps) {
+    const isDark = useRecoilValue(isDarkAtom);
+
     const { isLoading, data } = useQuery<IHistorycal[]>(["ohlcv", coinId], () =>
         fetchCoinHistory(coinId), {
         refetchInterval: 10000,
@@ -38,7 +42,7 @@ function Chart({ coinId }: ChartProps) {
                     ]}
                     options={{
                         theme: {
-                            mode: "dark",
+                            mode: isDark ? "dark" : "light",
                         },
                         chart: {
                             height: 500,
