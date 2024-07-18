ea6160e0ef914f17b667102d4ea232f05f89e057
Bsh <dnfg12@naver.com>
2024-07-18 17:03:34 +0900

    react masterClass section Six 6.14Lecture

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -7,13 +7,31 @@ function ToDo({ text, category, id }: IToDo) {
         const {
             currentTarget: { name },
         } = event;
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+            const oldToDo = oldToDos[targetIndex];
+            const newToDo = { text, id, category: name };
+            return oldToDos;
+        });
     };
     return (
         <li>
             <span>{text}</span>
-            {category !== "DOING" && <button onClick={onClick}>Doing</button>}
-            {category !== "DOING" && <button onClick={onClick}>To Do</button>}
-            {category !== "DOING" && <button onClick={onClick}>Done</button>}
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>
+                    Doing
+                </button>
+            )}
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>
+                    To Do
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>
+                    Done
+                </button>
+            )}
         </li>
     );
 }
