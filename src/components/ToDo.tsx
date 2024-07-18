e4a78864ab8fa2da46827ddf4c36e8775e90a70d
Bsh <dnfg12@naver.com>
2024-07-18 16:34:29 +0900

    react masterClass section Six 6.13 Lecture

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,19 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget: { name },
+        } = event;
+    };
     return (
         <li>
             <span>{text}</span>
-            <button>To Do</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {category !== "DOING" && <button onClick={onClick}>Doing</button>}
+            {category !== "DOING" && <button onClick={onClick}>To Do</button>}
+            {category !== "DOING" && <button onClick={onClick}>Done</button>}
         </li>
     );
 }
