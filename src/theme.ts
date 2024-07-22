305078d77a221afdb15b2816cbe04491abe2c532
Bsh <dnfg12@naver.com>
2024-07-22 15:39:52 +0900

    react masterClass section Seven 7.2 Lecture

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components/dist/types";
 
 export const darkTheme: DefaultTheme = {
-  textColor: "black",
+  textColor: "white",
   bgColor: "#2f3640",
   accentColor: "purple",
 };
