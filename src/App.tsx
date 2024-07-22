892d44c57ce6b575e7c3e611c08d9e259edcadaf
Bsh <dnfg12@naver.com>
2024-07-22 18:19:40 +0900

    react masterClass section Seven 7.7 Lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import {
+	DragDropContext,
+	Draggable,
+	Droppable,
+	DropResult,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
+import DraggabbleCard from "./components/DragabbleCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -25,17 +33,17 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
-
-const toDos = ["a", "b", "c", "d", "e", "f"];
-
 function App() {
-	const onDragEnd = () => { };
+	const [toDos, setToDos] = useRecoilState(toDoState);
+	const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+		if (!destination) return;
+		setToDos((oldToDos) => {
+			const copyToDos = [...oldToDos];
+			copyToDos.splice(source.index, 1);
+			copyToDos.splice(destination.index, 0, draggableId);
+			return copyToDos;
+		});
+	};
 	return (
 		<DragDropContext onDragEnd={onDragEnd}>
 			<Wrapper>
@@ -44,17 +52,7 @@ function App() {
 						{(magic) => (
 							<Board ref={magic.innerRef} {...magic.droppableProps}>
 								{toDos.map((toDo, index) => (
-									<Draggable draggableId={toDo} index={index}>
-										{(magic) => (
-											<Card
-												ref={magic.innerRef}
-												{...magic.dragHandleProps}
-												{...magic.draggableProps}
-											>
-												{toDo}
-											</Card>
-										)}
-									</Draggable>
+									<DraggabbleCard key={toDo} index={index} toDo={toDo} />
 								))}
 								{magic.placeholder}
 							</Board>
