5a6b868acfff1773a143702b1e2e5c36b92a608c
Bsh <dnfg12@naver.com>
2024-07-23 14:12:36 +0900

    react masterClass section Seven 7.12 Lecture

--- src/components/DragabbleCard.tsx
+++ src/components/DragabbleCard.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   margin-bottom: 5px;
   padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  background-color: ${(props) =>
+        props.isDragging ? "#74b9ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+        props.isDragging ? "0px 2px 5px rgba(15,15,15,0.5)" : "none"};
 `;
 
 interface IDragabbleCardProps {
@@ -18,8 +21,9 @@ function DraggabbleCard({ toDo, index }: IDragabbleCardProps) {
     console.log(toDo, "is reRendered");
     return (
         <Draggable key={toDo} draggableId={toDo} index={index}>
-            {(magic) => (
+            {(magic, snapshot) => (
                 <Card
+                    isDragging={snapshot.isDragging}
                     ref={magic.innerRef}
                     {...magic.dragHandleProps}
                     {...magic.draggableProps}
