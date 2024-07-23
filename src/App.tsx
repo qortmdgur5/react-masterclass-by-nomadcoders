a76181561f0ef493af97aca30239e43aa494ad31
Bsh <dnfg12@naver.com>
2024-07-23 12:20:49 +0900

    react masterClass section Seven 7.10 Lecture

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,9 @@ const Boards = styled.div`
 function App() {
 	const [toDos, setToDos] = useRecoilState(toDoState);
 	const onDragEnd = (info: DropResult) => {
-		console.log(info);
 		const { destination, draggableId, source } = info;
-		if (destination?.droppableId === source.droppableId) {
+		if (!destination) return;
+		if (destination.droppableId === source.droppableId) {
 			// same board movement.
 			setToDos((allBoards) => {
 				const boardCopy = [...allBoards[source.droppableId]];
@@ -37,6 +37,19 @@ function App() {
 					[source.droppableId]: boardCopy,
 				};
 			});
+		}
+		if (destination.droppableId != source.droppableId) {
+			setToDos((allBoards) => {
+				const sourceBoard = [...allBoards[source.droppableId]];
+				const destinationBoard = [...allBoards[destination.droppableId]];
+				sourceBoard.splice(source.index, 1);
+				destinationBoard.splice(destination?.index, 0, draggableId);
+				return {
+					...allBoards,
+					[source.droppableId]: sourceBoard,
+					[destination.droppableId]: destinationBoard
+				}
+			})
 		}
 	};
 	return (
