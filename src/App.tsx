e4fd2c52a27b5cc0ea2a1e91861a995b4a66c4aa
Bsh <dnfg12@naver.com>
2024-07-25 18:15:58 +0900

    react masterClass NONFLIX CLONE 9.1 Lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
-
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
-	return (
-		<Wrapper>
-			<Box
-				transition={{ type: "spring", bounce: 0.1 }}
-				initial={{ scale: 0 }}
-				animate={{ scale: 1, rotateZ: 360 }}
-			/>
-		</Wrapper>
-	);
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
