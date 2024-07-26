06075f1856659686662d5004f5f08c13735739b3
Bsh <dnfg12@naver.com>
2024-07-26 15:27:09 +0900

    react masterClass NOMFLIX CLONE 9.5 Lecture

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "7ef55f84f5fd72258db4251bf7369ae0";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
