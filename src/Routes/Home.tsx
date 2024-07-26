37860607cc7308d1aca28a47583ac5921f20b5c5
Bsh <dnfg12@naver.com>
2024-07-26 17:15:11 +0900

    react masterClass NOMFLIX CLONE 9.9 Lecture

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,13 @@
 import { useQuery } from "react-query";
-import { getMovies, IGetMoviesResult } from "../api";
 import styled from "styled-components";
-import { makeImagePath } from "../uills";
+import { motion, AnimatePresence, delay } from "framer-motion";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "../utils";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background: black;
+  padding-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -27,7 +30,7 @@ const Banner = styled.div<{ bgPhoto: string }>`
 
 const Title = styled.h2`
   font-size: 68px;
-  margin-bottom: 20px;
+  margin-bottom: 20px; ;
 `;
 
 const Overview = styled.p`
@@ -35,25 +38,121 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div) <{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  font-size: 66px;
+  &:first-child{
+    transform-origin: center left;
+  }
+  &:last-child{
+    transform-origin: center right;
+  }
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth + 5,
+    },
+    visible: {
+        x: 0,
+    },
+    exit: {
+        x: -window.outerWidth - 5,
+    },
+};
+
+const boxVariants = {
+    normal: {
+        scale: 1,
+    },
+    hover: {
+        scale: 1.3,
+        y: -50,
+        transition: {
+            delay: 0.3,
+            type: "tween"
+        }
+    },
+}
+
+const offset = 6;
+
 function Home() {
     const { data, isLoading } = useQuery<IGetMoviesResult>(
         ["movies", "nowPlaying"],
         getMovies
     );
+    const [index, setIndex] = useState(0);
+    const [leaving, setLeaving] = useState(false);
+    const incraseIndex = () => {
+        if (data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalMovies = data.results.length - 1;
+            const maxIndex = Math.floor(totalMovies / offset) - 1;
+            setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+        }
+    };
+    const toggleLeaving = () => setLeaving((prev) => !prev);
     return (
         <Wrapper>
             {isLoading ? (
                 <Loader>Loading...</Loader>
             ) : (
                 <>
-                    <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                    <Banner
+                        onClick={incraseIndex}
+                        bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                    >
                         <Title>{data?.results[0].title}</Title>
                         <Overview>{data?.results[0].overview}</Overview>
                     </Banner>
+                    <Slider>
+                        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                            <Row
+                                variants={rowVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                transition={{ type: "tween", duration: 1 }}
+                                key={index}
+                            >
+                                {data?.results
+                                    .slice(1)
+                                    .slice(offset * index, offset * index + offset)
+                                    .map((movie) => (
+                                        <Box
+                                            key={movie.id}
+                                            initial="normal"
+                                            whileHover="hover"
+                                            variants={boxVariants}
+                                            transition={{ type: "tween" }}
+                                            bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                        />
+                                    ))}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
                 </>
             )}
         </Wrapper>
     );
 }
-
 export default Home;
